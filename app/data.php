0fa2cd52f3020b31d20ab98d8b364fdab6efb4d9
sergiomitbo <smb0008@alu.medac.es>
2025-11-13 14:08:53 +0100

    tarea medio terminada

---
+++ app/data.php
@@ -0,0 +1,15 @@
+<?php
+// app/data.php
+$usuarios_bbdd = [
+'usuario1@taskflow.com' => [
+'id' => 1,
+'nombre' => 'Oskar Calvo',
+'password' => 'pass123' // Simulación de contraseña (sin hashear,por ahora)
+],
+'usuario2@taskflow.com' => [
+'id' => 2,
+'nombre' => 'Ana Alumna',
+'password' => 'admin'
+]
+];
+?>
--- taskflow/app/views/footer.php
+++
@@ -1,6 +0,0 @@
- </main>
-    <footer>
-        <p>© <?php echo date('Y'); ?> TaskFlow</p>
-    </footer>
-</body>
-</html>
