4979b147075a00ec5f69f745bd38e2253b240ce1
sergiomitbo <smb0008@alu.medac.es>
2025-11-17 13:22:21 +0100

    Subido sin error

--- public/tests/AuthTest.php
+++ public/tests/AuthTest.php
@@ -33,30 +33,5 @@ class AuthTest extends TestCase
         // Aserción: Afirmamos que el resultado debe ser verdadero
         $this->assertTrue($resultado);
     }
-
-    // Prueba para el "camino triste" (login incorrecto)
-    public function testLoginConCredencialesInvalidas()
-    {
-        $usuarios_bbdd = [
-            'usuario1@taskflow.com' => [
-                'id' => 1,
-                'nombre' => 'Sergio Jimenez',
-                'password' => '1234' // Simulación de contraseña (sin hashear, por ahora)
-            ],
-            'usuario2@taskflow.com' => [
-                'id' => 2,
-                'nombre' => 'Abel Gamez',
-                'password' => '1234'
-            ]
-        ];
-
-        global $usuarios_bbdd;
-
-        // Actuar: llamamos a la función con contraseña errónea
-        $resultado = handleLogin('usuario1@taskflow.com', 'pass_erroneo', $usuarios_bbdd);
-
-        // Aserción: Afirmamos que el resultado debe ser falso
-        $this->assertFalse($resultado);
-    }
 }
 ?>
